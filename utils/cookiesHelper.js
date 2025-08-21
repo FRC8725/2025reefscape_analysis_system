866cb94e89500fca063150aeec0f8cc0a7f14d3a
XxAlanXDxX <alanwu0517+github@gmail.com>
2025-08-21 15:59:49 +0800

    Improves authentication with cookie storage
    Migrates authentication token storage from localStorage to cookies for enhanced security and accessibility.
    This change also includes updated script ID, cookie helper utilities and disables auto select on logout.

---
+++ utils/cookiesHelper.js
@@ -0,0 +1,23 @@
+﻿export function setCookie(name, value, days) {
+    const d = new Date();
+    d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + d.toUTCString();
+    document.cookie = name + "=" + encodeURIComponent(value) + ";" + expires + ";path=/";
+}
+
+export function getCookie(name) {
+    const cname = name + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i].trim();
+        if (c.indexOf(cname) === 0) {
+            return c.substring(cname.length, c.length);
+        }
+    }
+    return "";
+}
+
+export function deleteCookie(name) {
+    document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+}
